22e317c000267f5d24e99b3e237fa7f0dd31c9b3
Jeija <norrepli@gmail.com>
2014-12-22 11:25:50 +0100

    Add comment field to simple transaction userclient

--- userclient/site/transaction_simple/scripts.js
+++ userclient/site/transaction_simple/scripts.js
@@ -1,10 +1,15 @@
 var ZBARCAM = "zbarcam";
 var WEBCAM = "/dev/video0";
 var ZBC_FLAGS = "--prescale=640x480"
+var COMMENT_MAXLEN = 300
 
 var process = require("child_process");
 
 $(function () {
+	action("config_get", "tr_comment_maxlen", function (res) {
+		if (res != "") COMMENT_MAXLEN = parseInt(res);
+	});
+
 	$("#header").load("../header.html", function () {
 		$("#transaction_simple_link").addClass("link-selected");
 	});
@@ -20,5 +25,15 @@ $(function () {
 				zbar.kill();
 			});
 		});
+	});
+
+	$(".comment").on("input", function () {
+		if ($(".comment").val().length > COMMENT_MAXLEN) {
+			$(this).css("box-shadow", "0 0 10px #f33");
+			$(".comment_info").css("color", "#f00");
+		} else {
+			$(this).css("box-shadow", "none");
+			$(".comment_info").css("color", "#fff");
+		}
 	});
 });
