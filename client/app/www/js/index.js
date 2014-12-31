fc7a5c084b465ff081d1b343e9a95851b5997694
Jeija <norrepli@gmail.com>
2014-12-31 17:20:10 +0100

    Implement proof-of-concept QR-ID scanner, persistent storage. Remove PhoneGap, use Apache Cordova only. Add flashlight, barcodescanner and splashscreen plugins as git submodules.

--- client/app/www/js/index.js
+++ client/app/www/js/index.js
@@ -1,17 +1,50 @@
-/* TODO
-
-document.addEventListener("deviceready"; onDeviceReady, false);
-
-function onDeviceReady() {
-document.getElementById("Info").innerHTML = "App geladen!";
-}
-*/
-
-
-function myFunction() {
-document.getElementById("Info").innerHTML = "Geladen!";
+function flashLightOn() {
+	window.plugins.flashlight.available(function(isAvailable) {
+		if (isAvailable) {
+			window.plugins.flashlight.switchOn();
+		}
+	});
 }
 
-window.setTimeout(function() {
-    window.location.href = 'main.html';
-}, 1000);
+function flashLightOff() {
+	window.plugins.flashlight.available(function(isAvailable) {
+		if (isAvailable) {
+			window.plugins.flashlight.switchOn();
+		}
+	});
+}
+
+$(function () {
+
+$("#authenticate").click(function () {
+	if (cordova.plugins.barcodeScanner) {
+		cordova.plugins.barcodeScanner.scan(
+			function (result) {
+				if (result.format != "QR_CODE") {
+					alert("Ungültiger Code!");
+					return;
+				}
+				storage.set("qrid", result.text);
+				alert(storage.get("qrid"));
+
+				var queryData = JSON.stringify({
+					qrid : result.text
+				});
+
+				action("student_identify", queryData, function (res) {
+					var st = undefined;
+					try {
+						st = JSON.parse(res);
+					} catch (e) {
+						alert("Der QR-Code ist ungültig");
+					}
+
+					alert("Dein Name ist " + st.firstname + " " + st.lastname);
+				});
+			}, 
+			function (error) {}
+		);
+	} // TODO: use JS QR Scanner if plugin doesn't exist
+});
+
+});
---
+++ client/app/www/js/storage.js
@@ -0,0 +1,23 @@
+var storage = {
+	getStorage : function () {
+		var str = window.localStorage.getItem("sasapp");
+		var data;
+
+		try {
+			return JSON.parse(str) || {};
+		} catch(e) {
+			return {};
+		}
+	},
+
+	set : function (key, value) {
+		var data = storage.getStorage();
+		data[key] = value;
+		window.localStorage.setItem("sasapp", JSON.stringify(data));
+	},
+
+	get : function (key) {
+		var data = storage.getStorage();
+		return data[key];
+	}
+}
