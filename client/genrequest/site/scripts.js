3de05c83599f181fc065e1d42d8af6ad28930306
Jeija <norrepli@gmail.com>
2015-01-26 20:51:40 +0100

    Functionality of 5ce8c95 for actions that don't require certificates

--- client/genrequest/site/scripts.js
+++ client/genrequest/site/scripts.js
@@ -1,3 +1,8 @@
+function printMessageReadable(msg) {
+		var res_readable = JSON.stringify(msg).replace(/(.{60})/g, "$1\n");
+		$("#response").text(res_readable);
+}
+
 function getMessage() {
 	var msg;
 	try {
@@ -11,14 +16,13 @@ function getMessage() {
 $(function () {
 	$("#confirm").click(function () {
 		action($("#action_name").val(), getMessage(), function (res) {
-			$("#response").text(res);
+			printMessageReadable(res);
 		});
 	});
 
 	$("#confirm_cert").click(function () {
 		action_mastercert($("#action_name").val(), getMessage(), "#cert_input", function (res) {
-			var res_readable = JSON.stringify(res).replace(/(.{60})/g, "$1\n");
-			$("#response").text(res_readable);
+			printMessageReadable(res);
 		});
 	});
 });
