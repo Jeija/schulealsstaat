8b7b5c6743124a40339fac76b5070dfb544cb6e5
Jeija <norrepli@gmail.com>
2015-02-22 10:53:56 +0100

    WiFi Redirect Page: Trick Android into believing it is on the public internet (circumvent walled garden checking)

--- network/illegal/server.js
+++ network/illegal/server.js
@@ -6,14 +6,17 @@ var index = fs.readFileSync(path.join(__dirname, "site", "index.html"));
 var euflag = fs.readFileSync(path.join(__dirname, "site", "euflag.svg"));
 var styles = fs.readFileSync(path.join(__dirname, "site", "styles.css"));
 http.createServer(function (req, res) {
-	if (req.url.indexOf("euflag.svg") > -1) {
+	if (req.url.indexOf("generate_204") > -1) {
+		// Trick Android into believing it is on the public internet
+		res.writeHead(204);
+		res.end();
+	} else if (req.url.indexOf("euflag.svg") > -1) {
 		res.writeHead(200, { "Content-Type" : "image/svg+xml" });
 		res.end(euflag);
 	} else if (req.url.indexOf("styles.css") > -1) {
 		res.writeHead(200, { "Content-Type" : "text/css" });
 		res.end(styles);
-	}
-	else {
+	} else {
 		res.writeHead(200, { "Content-Type" : "text/html" });
 		res.end(index);
 	}
