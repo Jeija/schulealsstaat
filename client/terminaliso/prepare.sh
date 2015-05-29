0e7e1283f091a4f379fc56223a34e827676cf620
Jeija <norrepli@gmail.com>
2015-05-29 11:59:45 +0200

    Terminaliso: Add archiso to build dependencies

--- client/terminaliso/prepare.sh
+++ client/terminaliso/prepare.sh
@@ -4,7 +4,7 @@ set -e
 
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo pacman -Sy --noconfirm --needed devtools arch-install-scripts wget
+sudo pacman -Sy --noconfirm --needed devtools arch-install-scripts wget archiso
 
 $CWD/localrepo.sh
 $CWD/genkeys.sh
