e3ac276d898e1ea5ba9ac35d6542798bd0e74925
Jeija <norrepli@gmail.com>
2015-06-02 13:03:20 +0200

    API: Online Gateway: Retry SSH tunnel in case port forward fails (ExitOnForwardFailure)

--- server/online_proxy_ssh.sh
+++ server/online_proxy_ssh.sh
@@ -10,7 +10,6 @@
 # on the client and the signature on the remote server.
 
 set -e
-
 if [ -z "$3" ]; then
 	echo "You must specify the local URL of the API server to use"
 	echo "and the SSH key to use."
@@ -27,9 +26,13 @@ REMOTE_PORT=1230
 LOCAL_SERVER=$2
 LOCAL_PORT=1230
 SSH_KEYFILE=$3
+set +e
 
-ssh -f -o StrictHostKeyChecking=no -i $SSH_KEYFILE $REMOTE_USER@$REMOTE_SERVER "$RESET_COMMAND"
-sleep 5
-autossh -M 0 -N -R \*:$REMOTE_PORT:$LOCAL_SERVER:$LOCAL_PORT \
-	$REMOTE_USER@$REMOTE_SERVER -i $SSH_KEYFILE -v \
-	-o "ServerAliveInterval 10" -o "ServerAliveCountMax 2"
+while true; do
+	ssh -f -o StrictHostKeyChecking=no -i $SSH_KEYFILE $REMOTE_USER@$REMOTE_SERVER "$RESET_COMMAND"
+	sleep 5
+	autossh -M 0 -N -R \*:$REMOTE_PORT:$LOCAL_SERVER:$LOCAL_PORT \
+		$REMOTE_USER@$REMOTE_SERVER -i $SSH_KEYFILE -v \
+		-o "ServerAliveInterval 10" -o "ServerAliveCountMax 2" \
+		-o "ExitOnForwardFailure=yes"
+done
