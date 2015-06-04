cf12e585f1d4f095d37dbffb47da2b35ea1e63d7
Jeija <norrepli@gmail.com>
2015-06-04 12:44:06 +0200

    Adminpanel: Improve Design of ID Cards and handle very long names

--- client/adminpanel/site/idcards/scripts.js
+++ client/adminpanel/site/idcards/scripts.js
@@ -11,33 +11,45 @@ var PAGE_OFFSET_Y = 21;
 
 // QR Code
 var QRCODE_OFFSET_X = 4;
-var QRCODE_OFFSET_Y = 2;
+var QRCODE_OFFSET_Y = 7;
 var QRCODE_W = 41;
 var QRCODE_H = 41;
 var QRCODE_RESOLUTION = 10;
 
 // Passport Image
 var PASSPIC_OFFSET_X = 43;
-var PASSPIC_OFFSET_Y = 2;
-var PASSPIC_W = 21.333;
-var PASSPIC_H = 16;
+var PASSPIC_OFFSET_Y = 7;
+var PASSPIC_W = 23;
+var PASSPIC_H = 17.25;
 
 // Text
-var TEXT_OFFSET_X = 43;
-var TEXT_OFFSET_Y = 28;
-var TEXT_FONTSIZE = 8;
+var TEXT_OFFSET_X = 51;
+var TEXT_OFFSET_Y = 28.5;
+var TEXT_FONTSIZE = 6;
 var TEXT_LINEHEIGHT = 3;
 
+// Title
+var TITLE_OFFSET_X = CARD_W / 2;
+var TITLE_OFFSET_Y = 5;
+var TITLE_FONTSIZE = 13;
+var TITLE_FONTSIZE_TOOLONG = 8;
+
 // QR-ID
-var QRID_OFFSET_X = 16;
-var QRID_OFFSET_Y = 47;
+var QRID_OFFSET_X = QRCODE_OFFSET_X + QRCODE_W / 2;
+var QRID_OFFSET_Y = 49;
 var QRID_FONTSIZE = 10;
 
 // Logo
-var LOGO_OFFSET_X = 4;
-var LOGO_OFFSET_Y = 41;
-var LOGO_W = 9;
-var LOGO_H = 9;
+var LOGO_OFFSET_X = 43.5;
+var LOGO_OFFSET_Y = 26;
+var LOGO_W = 6;
+var LOGO_H = 6;
+
+// Wunderland Logo
+var WUNDERLAND_OFFSET_X = 43;
+var WUNDERLAND_OFFSET_Y = 35;
+var WUNDERLAND_W = 22.96;
+var WUNDERLAND_H = 8.568;
 
 var AMOUNT_X = Math.floor((PAGE_W - PAGE_OFFSET_X + CARD_MARGIN_X) / (CARD_W + CARD_MARGIN_X));
 var AMOUNT_Y = Math.floor((PAGE_H - PAGE_OFFSET_Y + CARD_MARGIN_Y) / (CARD_H + CARD_MARGIN_Y));
@@ -64,10 +76,45 @@ function toJPEG (src, cb) {
 	};
 }
 
+/**
+ * Taken from http://stackoverflow.com/questions/21060876/is-there-any-way-to-center-text-with-jspdf,
+ * user Tsilis
+ **/
+(function(API) {
+	API.centerText = function(x, y, txt) {
+		var fontSize = this.internal.getFontSize();
+		var pageWidth = this.internal.pageSize.width;
+		txtWidth = this.getStringUnitWidth(txt) * fontSize / this.internal.scaleFactor;
+		x = x - txtWidth / 2;
+		this.text(txt, x, y);
+	}
+})(jsPDF.API);
+
 var saeulogo = null;
 toJPEG("saeulogo.png", function (jpeg) {
 	saeulogo = jpeg;
 });
+
+var wunderland = null;
+toJPEG("wunderland.png", function (jpeg) {
+	wunderland = jpeg;
+});
+
+var passpic_visitors = null;
+toJPEG("passpic_visitors.png", function (jpeg) {
+	passpic_visitors = jpeg;
+});
+
+var passpic_legalentity = null;
+toJPEG("passpic_legalentity.png", function (jpeg) {
+	passpic_legalentity = jpeg;
+});
+
+var passpic_replacement = null;
+toJPEG("passpic_replacement.png", function (jpeg) {
+	passpic_replacement = jpeg;
+});
+
 
 function assert(key, object) {
 	if (!(key in object)) throw key + " fehlt!";
@@ -78,45 +125,56 @@ function date_readable (birth) {
 	return bdate.getDate() + "." + (bdate.getMonth() + 1) + "." + bdate.getFullYear();
 }
 
+function country_readable (countrycode) {
+	if (countrycode == "de") return "Deutschland";
+	if (countrycode == "fr") return "Frankreich";
+	if (countrycode == "tr") return "Türkei";
+	if (countrycode == "gb") return "Großbritannien";
+	if (countrycode == "it") return "Italien";
+	return "Kein Land";
+}
+
 /**
- * Returns multiple lines of text that describe a student / an account,
- * e.g. containing name, class, birth date, etc.
- * Return value is in the form:
- *	[ line0, line1, line2, ... ]
+ * Returns title and lines of additional information per account:
+ * {
+ *	title : String,
+ *	lines : [ String, String, ... ]
+ * }
  */
 function student_readable(student) {
+	var title = "";
 	var lines = [];
 	if (!("type") in student) throw "type is missing";
-	var cl = student.type.toUpperCase();
-	if (cl == "REPLACEMENT") {
-		lines.push("Ersatzkarte");
-	} else if (cl == "LEGALENTITY") {
+	var cl = student.type.toLowerCase();
+	if (cl == "replacement") {
+		title = "Name:                                      ";
+		lines.push("Ersatzkarte /");
+		lines.push("Sonderkarte");
+	} else if (cl == "legalentity") {
 		assert("special_name", student);
-		lines.push(student.special_name);
-		lines.push("(juristische Person)");
-	} else if (cl == "VISITOR") {
-		lines.push("Besucher");
-		lines.push("");
-		lines.push("Diese Karte darf");
-		lines.push("       NICHT");
-		lines.push("weitergegeben");
-		lines.push("werden.");
+		title = student.special_name;
+		lines.push("juristische");
+		lines.push("Person");
+	} else if (cl == "visitor") {
+		title = "Besucherausweis";
+		lines.push("Herzlich");
+		lines.push("Willkommen in...");
 	} else {
 		assert("firstname", student);
 		assert("lastname", student);
-		lines.push(student.firstname + " " + student.lastname);
-		if (cl == "TEACHER") {
+		title = student.firstname + " " + student.lastname;
+		if (cl == "teacher") {
 			lines.push("Lehrer / Lehrerin");
-		} else if (cl == "OTHER") {
-			if ("birth" in student)
-				lines.push("* " + date_readable(student.birth));
+		} else if (cl == "other") {
+			lines.push("Sonderausweis");
 		} else {
-			lines.push(cl);
+			lines.push("Klasse " + cl.toUpperCase());
 			if ("birth" in student)
 				lines.push("* " + date_readable(student.birth));
 		}
+		lines.push(country_readable(student.country ? student.country.toLowerCase() : ""));
 	}
-	return lines;
+	return { title : title, lines : lines };
 }
 
 function render_student_card(pdf, student, page, xpos, ypos) {
@@ -149,24 +207,46 @@ function render_student_card(pdf, student, page, xpos, ypos) {
 					ypos + PASSPIC_OFFSET_Y, PASSPIC_W, PASSPIC_H);
 			});
 		});
+	} else if (student.type == "visitor") {
+		pdf.addImage(passpic_visitors, "JPEG", xpos + PASSPIC_OFFSET_X,
+			ypos + PASSPIC_OFFSET_Y, PASSPIC_W, PASSPIC_H);
+	} else if (student.type == "legalentity") {
+		pdf.addImage(passpic_legalentity, "JPEG", xpos + PASSPIC_OFFSET_X,
+			ypos + PASSPIC_OFFSET_Y, PASSPIC_W, PASSPIC_H);
+	} else if (student.type == "replacement") {
+		pdf.addImage(passpic_replacement, "JPEG", xpos + PASSPIC_OFFSET_X,
+			ypos + PASSPIC_OFFSET_Y, PASSPIC_W, PASSPIC_H);
 	}
 
+	var readable = student_readable(student);
+
+	// Render Title
+	pdf.setFontSize(TITLE_FONTSIZE);
+	if (readable.title.length > 25) pdf.setFontSize(TITLE_FONTSIZE_TOOLONG);
+	pdf.setFont("helvetica", "bold");
+	pdf.centerText(xpos + TITLE_OFFSET_X, ypos + TITLE_OFFSET_Y, readable.title);
+
 	// Render Text
-	var lines = student_readable(student);
 	pdf.setFontSize(TEXT_FONTSIZE);
-	for (var l = 0; l < lines.length; l++) {
+	pdf.setFont("helvetica", "normal");
+
+	for (var l = 0; l < readable.lines.length; l++) {
 		pdf.text(xpos + TEXT_OFFSET_X, ypos + TEXT_OFFSET_Y + TEXT_LINEHEIGHT * l,
-			lines[l]);
+			readable.lines[l]);
 	}
 
 	// Render QR-ID
 	assert("qrid", student);
 	pdf.setFontSize(QRID_FONTSIZE);
-	pdf.text(xpos + QRID_OFFSET_X, ypos + QRID_OFFSET_Y, student.qrid);
+	pdf.centerText(xpos + QRID_OFFSET_X, ypos + QRID_OFFSET_Y, student.qrid);
 
 	// Render Logo
 	pdf.addImage(saeulogo, "JPEG", xpos + LOGO_OFFSET_X, ypos + LOGO_OFFSET_Y,
 		LOGO_W, LOGO_H);
+
+	// Render Wunderland
+	pdf.addImage(wunderland, "JPEG", xpos + WUNDERLAND_OFFSET_X, ypos +WUNDERLAND_OFFSET_Y,
+		WUNDERLAND_W, WUNDERLAND_H);
 }
 
 function render_students(pdf, list) {
@@ -215,7 +295,8 @@ $("#type_ok").click(function () {
 	var condition = {
 		query : { type : type },
 		fields : {"firstname" : 1, "type" : 1, "lastname" : 1,
-			"qrid" : 1, "picname" : 1, "birth" : 1, "special_name" : 1}
+			"qrid" : 1, "picname" : 1, "birth" : 1, "special_name" : 1,
+			"country" : 1}
 	};
 	if (type == "*") condition.query = {};
 	action_cert("students_get", condition, "admin_cert", function (res) {
@@ -237,5 +318,4 @@ $("#upload").click(function () {
 		reader.readAsText(e.target.files[0]);
 	});
 });
-
 });
