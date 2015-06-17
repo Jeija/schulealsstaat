e8ce1b09bd389e85cdbb3d9044cf180613416cf3
Jeija <norrepli@gmail.com>
2015-06-17 20:52:39 +0200

    Terminaliso: Add /tmp/nocamloop script, disables v4l2 loopback and symlinks /dev/video20 to the webcam device so that zbarcam re-opens the webcam stream each time it is lauched Some webcams apparently don't work with v4l2loopback that well, so this is an easy solution to manually disable the loopback for those devices.

--- client/terminaliso/dlpackage/basepkg/qrloopback.sh
+++ client/terminaliso/dlpackage/basepkg/qrloopback.sh
@@ -9,8 +9,23 @@ if [ "$FEED" = "ignore" ]; then
 	exec $0
 fi
 
+### Add script to just reopen webcam stream if builtin cam doesn't work with loopback ###
+cat << EOF > /tmp/nocamloop
+#!/bin/bash
+killall qrloopback.sh
+killall ffmpeg
+sleep 2
+modprobe -r v4l2loopback
+sleep 2
+rm -f /dev/video20
+ln -s $FEED /dev/video20
+EOF
+chmod +x /tmp/nocamloop
+
 ### Webcam Loopback on /dev/video20 ###
 LOOPBACK_VIDEO=20
+rm -f /dev/video$LOOPBACK_VIDEO
+sleep 1
 modprobe v4l2loopback video_nr="$LOOPBACK_VIDEO"
 
 echo "Using webcam device $FEED"
