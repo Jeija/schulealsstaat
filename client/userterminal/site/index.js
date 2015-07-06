Jeija/schulealsstaat

commit 4d2fd4e054d46b882631aa5488a0970af8ed84a9

Userterminal: Completely reload page when clicking the transaction link, fixes some bugs that occured after long usage of the terminal devices

client/userterminal/site/index.js
@@ -83,7 +83,9 @@ $(function () {
 	load_subdir("transaction_simple");
 
 	$(".mainlink").click(function () {
-		load_subdir($(this).data("subdir"));
+		var subdir = $(this).data("subdir");
+		if (subdir == "transaction_simple") window.location.reload();
+		else load_subdir(subdir);
 	});
 
 	$("#volume").on("input change", function () {

commit 170bbe4cc4dd63c5fca4555228bcd095dc264dd6

Fix issues caused by 4d2fd4e, such as changing radio volume Allow manually refreshing the userterminal page by clicking the volume slider five times within one second instead

client/userterminal/site/index.html
@@ -35,8 +35,6 @@
 	<div id="settings" hidden>
 		<div class="settings-heading">Preset Target QR-IDs (JSON)</div>
 		<input type="text" id="qrid_presets" value="{}">
-		<div class="settings-heading">Settings Password</div>
-		<input type="password" id="settings_password">
 		<input type="button" id="settings_exit" value="Exit">
 	</div>
 

client/userterminal/site/index.js
@@ -3,11 +3,11 @@ var childp = null;
 var mplayer = null;
 
 if (typeof require !== "undefined") {
-	PWD_REQUIRED = require("fs").readFileSync("/tmp/password");
 	var childp = require("child_process");
 }
 
 var QRID_PRESETS = {};
+
 var RADIO_STREAMURL = "http://radio.saeu:8000/stream.ogg";
 var RADIO_METAURL = "http://radio.saeu:8000/status-json.xsl";
 
@@ -84,8 +84,21 @@ $(function () {
 
 	$(".mainlink").click(function () {
 		var subdir = $(this).data("subdir");
-		if (subdir == "transaction_simple") window.location.reload();
-		else load_subdir(subdir);
+		load_subdir(subdir);
+	});
+
+	// 5x click on menu item reloads page
+	var clicknum_reset = 0;
+	$("#volume").click(function () {
+		console.log("click");
+		clicknum_reset++;
+		if (clicknum_reset >= 5) {
+			mplayer.kill();
+			window.location.reload();
+		}
+		setTimeout(function () {
+			clicknum_reset = 0;
+		}, 1000);
 	});
 
 	$("#volume").on("input change", function () {
@@ -110,14 +123,10 @@ $(function () {
 
 	$("#settings_exit").click(function () {
 		$("#settings").hide();
-		var password = $("#settings_password").val();
-		$("#settings_password").val("");
-		if (password == PWD_REQUIRED) {
-			try {
-				QRID_PRESETS = JSON.parse($("#qrid_presets").val());
-			} catch(e) {
-				alert(e);
-			}
+		try {
+			QRID_PRESETS = JSON.parse($("#qrid_presets").val());
+		} catch(e) {
+			alert(e);
 		}
 	});
 